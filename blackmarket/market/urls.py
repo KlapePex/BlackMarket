9e06a06346f460546ae76a2743ee3e31030da63a
KlapePex <klap@inbox.lv>
2021-07-10 11:31:35 +0300

    add views and fix product db

--- blackmarket/market/urls.py
+++ blackmarket/market/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from . import views
-from .views import UserRegisterView
+from .views import UserRegisterView, CreateProduct, UpdateProduct, DetailProduct, DeleteProduct, ProductList
 
 urlpatterns = [
 
     path('', views.main, name="main"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
-    path('market/', views.market, name="market"),
+    path('market/', ProductList.as_view(), name="market"),
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('market/create', CreateProduct.as_view(),  name='create_product'),
+    path('market/<int:pk>', DetailProduct.as_view(),  name='detail_product'),
+    path('market/<int:pk>/update', UpdateProduct.as_view(),  name='update_product'),
+    path('market/<int:pk>/delete', DeleteProduct.as_view(),  name='delete_product'),
 ]
--- blackmarket/market/views.py
+++ blackmarket/market/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .forms import RegistrationForm
+from .models import Product, Category, Profile
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.db.models import Q
 
 
 class UserRegisterView(CreateView):
@@ -9,6 +12,12 @@ class UserRegisterView(CreateView):
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
+class DeleteProduct(PermissionRequiredMixin, DeleteView):
+    template_name = 'delete_product.html'
+    model = Product
+    context_object_name = 'product'
+    success_url = reverse_lazy('products')
+    permission_required = 'market.delete_product'
 
 def cart(request):
     context = {}
@@ -20,11 +29,48 @@ def checkout(request):
     return render(request, 'market/checkout.html', context)
 
 
-def market(request):
-    context = {}
-    return render(request, 'market/checkout.html', context)
+class ProductList(ListView):
+    template_name = 'market/market.html'
+    model = Product
+    context_object_name = 'products'
+    def get_queryset(self):
+        search = self.request.GET.get('search', None)
+
+        if search:
+            queryset = Product.objects.filter(name__contains=search)
+            queryset = Product.objects.filter(Q(name__contains=search) | Q(category__name__contains=search))
+        else:
+            queryset = Product.objects.all()
+
+        return queryset
 
 
 def main(request):
     context = {}
     return render(request, 'market/main.html', context)
+
+class CreateProduct(PermissionRequiredMixin, CreateView):
+    template_name = 'market/create_product.html'
+    model = Product
+    success_url = reverse_lazy('product')
+    fields = '__all__'
+    permission_required = 'market.add_product'
+
+class DetailProduct(DetailView):
+    template_name = 'market/detail_product.html'
+    model = Product
+    context_object_name = 'product'
+
+class UpdateProduct(PermissionRequiredMixin, UpdateView):
+    template_name = 'market/update_product.html'
+    model = Product
+    success_url = reverse_lazy('product')
+    fields = '__all__'
+    context_object_name = 'product'
+    permission_required = 'market.change_product'
+    
+
+class DetailProfile(DetailView):
+    template_name = 'market/detail_profile.html'
+    model = Profile
+    context_object_name = 'profile'
